d2ce661434d99ea9206170577db20314b8ef4d0e
YazanAbdelJalil <yazanabdaljaleel5@gmail.com>
2022-11-01 16:54:30 +0200

    Update

---
+++ src/selctFarst.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selctFarst {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "D:\\my new driver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		Thread.sleep(3000);
+		
+		driver.get("http://127.0.0.1:5500/indix.html");
+		String myTitle=driver.getTitle();
+		System.out.println(myTitle);
+		
+		List <WebElement> myListOptions = driver.findElements(By.tagName("a"));
+		for(int i=0; i<myListOptions.size();i++) {
+		System.out.println(myListOptions.get(i).getText());
+		}
+	}
+
+}
